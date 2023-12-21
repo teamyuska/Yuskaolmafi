3a3df1637913444bebafd766a0f9770a505971bd
teamyuska <118346503+teamyuska@users.noreply.github.com>
2023-12-21 11:13:01 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
-import os
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = os.getenv("API_ID", "7948888")
+API_HASH = os.getenv("API_HASH", "48285ac512613754f7913b310e884222")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "AAEqr0qHkncbPANl4sIyY3Zsrz08qg88tkc")
 DATABASE_URL = os.getenv("DATABASE_URL")
-OWNER_ID = list({int(x) for x in os.environ.get("OWNER_ID", "").split()})
+OWNER_ID = os.getenv("OWNER_ID", "6033604536")
